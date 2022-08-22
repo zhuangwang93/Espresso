3db7eab454182f7e34749e16df6abf9d0ca2e12e
wang.zhuang <wang.zhuang@bytedance.com>
2022-08-23 04:38:21 +0800

    automatic installation

--- install.sh
+++ install.sh
@@ -10,23 +10,24 @@ git clone https://github.com/NVIDIA/apex
 cd apex
 git checkout d6b5ae5d04f531ff862f651e67f241fef88fd159
 pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+cd ..
 
 sudo apt-get update && sudo apt-get install libgl1 -y
 pip3 install opencv-python
 
+echo "download dataset for LSTM"
+mkdir ~/data
+cd examples/lstm
+bash getdata.sh
+
 echo "download model checkpoint for BERT-base"
-cd examples/BERT
+cd ../BERT
 cd ./dataset/checkpoint
 wget --content-disposition https://api.ngc.nvidia.com/v2/models/nvidia/bert_pyt_ckpt_base_qa_squad11_amp/versions/19.09.0/zip -O bert_pyt_ckpt_base_qa_squad11_amp_19.09.0.zip
 sudo apt install unzip
 unzip bert_pyt_ckpt_base_qa_squad11_amp_19.09.0.zip
 cd ../../ && mkdir -p results
 
-
-echo "download dataset for LSTM"
-mkdir ~/data
-cd ../lstm
-bash getdata.sh
 
 echo "download dataset for GPT-2"
 cd ~/data
